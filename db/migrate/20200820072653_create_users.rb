e95959d873726448140ea265effffb34224ff104
pairing_station_oiizes <shared.oiizes@gmail.com>
2020-08-20 10:27:01 +0300

    Adds a user model

---
+++ db/migrate/20200820072653_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.integer :age
+      t.string :birth_place
+
+      t.timestamps
+    end
+  end
+end
