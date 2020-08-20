61eab0aa989a85676ce0592940745a3d31a797b9
pairing_station_oiizes <shared.oiizes@gmail.com>
2020-08-20 10:55:12 +0300

    Adds routes for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'api/v1/users', to: 'api/v1/users#index'
+  get 'api/v1/users/:id', to: 'api/v1/users#show'
+  post 'api/v1/users', to: 'api/v1/users#create'
+  put 'api/v1/users/:id', to: 'api/v1/users#update'
+  delete 'api/v1/users/:id', to: 'api/v1/users#destroy'
 end
